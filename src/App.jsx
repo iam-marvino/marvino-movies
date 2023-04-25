41f6acb9e6812267e242e8cae394924085bf31d8
iam-marvino <108211420+iam-marvino@users.noreply.github.com>
2023-04-25 14:26:40 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ export default function App(){
   let [inputValue,setInputValue] = React.useState('')
 
   React.useEffect( () => {
-    searchMovies('iron man')
+    searchMovies('Black Panther')
   },[])
 
   let searchMovies = async (title) => {
